605007f1cfb6df376ca13e151116450a403abe47
dewansh07 <dewansh.2021@vitbhopal.ac.in>
2024-06-04 20:08:15 +0530

    chore: Update dependencies for internet connection check and Firebase integration

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:medicine/Reports.dart';
 import 'package:medicine/add_medicine.dart';
 import 'package:medicine/profile.dart';
@@ -19,6 +23,20 @@ class Add extends StatefulWidget {
 class _AddState extends State<Add> {
   int _selectedIndex = 1; // Changed default index to '1'
   int Index = 1;
+  late StreamSubscription subscription;
+  bool isDeviceConnected = false;
+  bool isAlertSet = false;
+
+  getConnectivity() =>
+      subscription = Connectivity().onConnectivityChanged.listen(
+        (ConnectivityResult result) async {
+          isDeviceConnected = await InternetConnectionChecker().hasConnection;
+          if (!isDeviceConnected && isAlertSet == false) {
+            showDialogBox();
+            setState(() => isAlertSet = true);
+          }
+        },
+      );
 
   final List<Widget> _screens = [
     const Center(child: Text('Home Screen')),
@@ -139,17 +157,18 @@ class _AddState extends State<Add> {
 
             switch (Index) {
               case 0:
-              // Navigate to home page
+                // Navigate to home page
                 break;
               case 1:
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => Medicine())); // Corrected indentation
+                        builder: (context) =>
+                            Medicine())); // Corrected indentation
                 break;
               case 2:
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Report()));
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Report()));
                 break;
             }
           });
@@ -160,7 +179,8 @@ class _AddState extends State<Add> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(Icons.home_rounded,
-                    size: 32, color: Color(0xFF665CF5)), // Corrected indentation
+                    size: 32,
+                    color: Color(0xFF665CF5)), // Corrected indentation
                 Text('Home', style: TextStyle(color: Colors.grey[600])),
               ],
             ),
@@ -186,6 +206,111 @@ class _AddState extends State<Add> {
         ],
       ),
     );
+  }
+
+  showDialogBox() {
+    return (BuildContext context) {
+      return Scaffold(
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: SizedBox(
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: AlertDialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      title: Center(
+                        child: Text(
+                          'Your device is not connected',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 22,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset('images/signalr.png'),
+                          SizedBox(height: 10),
+                          Text(
+                            'Connect your device with',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                      actions: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              width: 110,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Color(0xFF665CF5),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: IconButton(
+                                icon:
+                                    Icon(Icons.bluetooth, color: Colors.white),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  // Add your onPressed code here
+                                },
+                              ),
+                            ),
+                            Container(
+                              width: 110,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Color(0xFF665CF5),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: IconButton(
+                                icon: Icon(Icons.wifi, color: Colors.white),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  // Add your onPressed code here
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    };
   }
 }
 
